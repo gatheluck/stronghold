f06ec37f0c642c9eaa185396700541b12e100639
gatheluck <gatheluck+tech@gmail.com>
2020-06-22 16:33:00 +0900

    add utils.py

---
+++ libs/utils.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+base = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../')
+sys.path.append(base)
+
+import itertools
+import torch
+
+
+def parse_args(required_keys: set, input_args: dict) -> dict:
+    """
+    check if all required_keys are inculuded in input_args.
+    return parsed_args only inculudes required keys.
+
+    Args
+    - required_keys (set) : set of required keys for input_args
+    - input_args (dict)   : dict of input arugments
+    """
+    parsed_args = dict()
+
+    for k in required_keys:
+        if k not in input_args.keys():
+            raise ValueError('initial args are invalid.')
+        else:
+            parsed_args[k] = input_args[k]
+
+    return parsed_args
+
+
+def get_epoch_end_log(outputs: list) -> dict:
+    """
+    this function fill the gap between single theread and data.parallel.
+    form of outputs is List[Dict[str, Tensor]] or List[List[Dict[str, Tensor]]]
+    """
+    log = dict()
+
+    # if list is nested, flatten them.
+    if type(outputs[0]) is list:
+        outputs = [x for x in itertools.chain(*outputs)]
+
+    if 'log' in outputs[0].keys():
+        print(outputs[0]['log'].keys())
+        for key in outputs[0]['log'].keys():
+            val = torch.stack([x['log'][key] for x in outputs]).mean().cpu().item()
+            log[key + '_avg'] = val
+    else:
+        for key in outputs[0].keys():
+            val = torch.stack([x[key] for x in outputs]).mean().cpu().item()
+            log[key + '_avg'] = val
+
+    return log
