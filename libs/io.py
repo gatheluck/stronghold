96b2d256fcfb65bb321f1afaa9111a0965e57f89
gatheluck <gatheluck+tech@gmail.com>
2020-06-23 19:50:00 +0900

    add .ckpt for load_model

--- libs/io.py
+++ libs/io.py
@@ -6,4 +6,17 @@ def save_model(model, path):
 
 
 def load_model(model, path):
-    model.load_state_dict(torch.load(path))
+    print('loading model weight from {path}'.format(path=path))
+
+    # load weight from .pth file.
+    if path.endswith('.pth'):
+        model.load_state_dict(torch.load(path))
+    # load weight from checkpoint.
+    elif path.endswith('.ckpt'):
+        checkpoint = torch.load(path)
+        if 'state_dict' in checkpoint.keys():
+            model.load_state_dict(checkpoint['state_dict'], strict=False)
+        else:
+            raise ValueError('this checkponint do not inculdes state_dict')
+    else:
+        raise ValueError('path is not supported type of extension.')
