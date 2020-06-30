049577f1b7b525acb7a913c56bd3c849c767ce91
gatheluck <gatheluck+tech@gmail.com>
2020-06-30 13:37:31 +0900

    add function

--- apps/test.py
+++ apps/test.py
@@ -15,14 +15,18 @@ from libs.io import save_model
 from libs.io import load_model
 from libs.io import log_hyperparams
 from libs.litmodel import LitModel
+from libs.utils import check_required_keys
 
 from submodules.ModelBuilder.model_builder import ModelBuilder
 
 
 @hydra.main(config_path='../conf/test.yaml', strict=False)
 def main(cfg: omegaconf.DictConfig) -> None:
-    if 'weight' not in cfg.keys():
-        raise KeyError('please specify weight option.')
+    # check config
+    required_keys = 'weight'.split()
+    check_required_keys(required_keys, cfg)
+
+    # fix weight bacause hydra change the current working dir
     cfg.weight = os.path.join(hydra.utils.get_original_cwd(), cfg.weight)
 
     hydra_logger = logging.getLogger(__name__)
@@ -55,13 +59,14 @@ def main(cfg: omegaconf.DictConfig) -> None:
         weights_save_path='.'
     )
 
+    # build model
     model = ModelBuilder(num_classes=cfg.dataset.num_classes, pretrained=False)[cfg.arch]
     if 'weight' in cfg:
-        print('loading weight from {weight}'.format(weight=cfg.weight))
+        hydra_logger.info('loading weight from {weight}'.format(weight=cfg.weight))
         load_model(model, cfg.weight)  # load model weight
     model.eval()
+
     # test
-    # litmodel = LitModel(cfg).load_from_checkpoint(model, cfg.ckpt, cfg)
     litmodel = LitModel(model, cfg)
     trainer.test(litmodel)
 
