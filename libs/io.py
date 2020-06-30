c70d9af10ae8883af87196ea581a662a01ad405d
gatheluck <gatheluck+tech@gmail.com>
2020-06-30 11:21:29 +0900

    add log_hyperparams function

--- libs/io.py
+++ libs/io.py
@@ -1,4 +1,6 @@
+import copy
 import torch
+import omegaconf
 
 
 def save_model(model, path):
@@ -20,3 +22,18 @@ def load_model(model, path):
             raise ValueError('this checkponint do not inculdes state_dict')
     else:
         raise ValueError('path is not supported type of extension.')
+
+
+def log_hyperparams(logger, cfg: omegaconf.DictConfig):
+    """
+    log hyperparameters from omegaconf.
+    because omegaconf is nested, this function is flatten them.
+    """
+    _cfg = copy.deepcopy(cfg)
+    for key, val in cfg.items():
+        if type(val) is omegaconf.dictconfig.DictConfig:
+            dict_for_log = {'.'.join([key, k]): v for k, v in val.items()}  # because cfg is nested dict, the nest info is added to keys.
+            logger.log_hyperparams(dict_for_log)
+            _cfg.pop(key)
+    logger.log_hyperparams(dict(_cfg))
+    del _cfg
