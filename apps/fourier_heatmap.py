2827d05831078d99a3bc770b7294694870deccd2
gatheluck <gatheluck+tech@gmail.com>
2020-06-30 15:47:36 +0900

    update logger

--- apps/fourier_heatmap.py
+++ apps/fourier_heatmap.py
@@ -6,11 +6,13 @@ sys.path.append(base)
 
 import copy
 import hydra
+import logging
 import omegaconf
 import pytorch_lightning
 
 from libs.utils import check_required_keys
 from libs.io import load_model
+from libs.io import log_hyperparams
 
 from submodules.DatasetBuilder.dataset_builder import DatasetBuilder
 from submodules.ModelBuilder.model_builder import ModelBuilder
@@ -19,34 +21,36 @@ from submodules.FourierHeatmap.fhmap.fourier_heatmap import create_fourier_heatm
 
 @hydra.main(config_path='../conf/fourier_heatmap.yaml', strict=False)
 def main(cfg: omegaconf.DictConfig) -> None:
-    print(cfg.pretty())
-
     # check config
     required_keys = 'weight'.split()
     check_required_keys(required_keys, cfg)
-    print(cfg.pretty())
 
     # fix weight bacause hydra change the current working dir
     cfg.weight = os.path.join(hydra.utils.get_original_cwd(), cfg.weight)
 
-    logger = pytorch_lightning.loggers.mlflow.MLFlowLogger(
-        experiment_name='mlflow_output',
-        tags=None
-    )
+    hydra_logger = logging.getLogger(__name__)
+    hydra_logger.info(' '.join(sys.argv))
+    hydra_logger.info(cfg.pretty())
+
+    loggers = [
+        pytorch_lightning.loggers.mlflow.MLFlowLogger(
+            experiment_name='mlflow_output',
+            tags=None)
+    ]
+    api_key = os.environ.get('ONLINE_LOGGER_API_KEY')
+    if api_key and cfg.online_logger.activate:
+        loggers.append(
+            pytorch_lightning.loggers.CometLogger(api_key=api_key)
+        )
 
     # log hyperparams
-    _cfg = copy.deepcopy(cfg)
-    for key, val in cfg.items():
-        if type(val) is omegaconf.dictconfig.DictConfig:
-            dict_for_log = {'.'.join([key, k]): v for k, v in val.items()}  # because cfg is nested dict, the nest info is added to keys.
-            logger.log_hyperparams(dict_for_log)
-            _cfg.pop(key)
-    logger.log_hyperparams(dict(_cfg))
+    for logger in loggers:
+        log_hyperparams(logger, cfg)
 
     # build model
     model = ModelBuilder(num_classes=cfg.dataset.num_classes, pretrained=False)[cfg.arch].cuda()
     if 'weight' in cfg:
-        print('loading weight from {weight}'.format(weight=cfg.weight))
+        hydra_logger.info('loading weight from {weight}'.format(weight=cfg.weight))
         load_model(model, cfg.weight)  # load model weight
 
     # prepare dataset builder
