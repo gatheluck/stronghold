957e9b0a318e787daeabbc5665f2de02d07c4433
gatheluck <gatheluck+tech@gmail.com>
2020-06-30 11:20:54 +0900

    add online_logging

--- apps/test.py
+++ apps/test.py
@@ -6,34 +6,43 @@ sys.path.append(base)
 
 import copy
 import hydra
+import logging
 import omegaconf
 import torch
 import pytorch_lightning
 
 from libs.io import save_model
+from libs.io import load_model
+from libs.io import log_hyperparams
 from libs.litmodel import LitModel
+
+from submodules.ModelBuilder.model_builder import ModelBuilder
 
 
 @hydra.main(config_path='../conf/test.yaml', strict=False)
 def main(cfg: omegaconf.DictConfig) -> None:
-    if 'ckpt_path' not in cfg.keys():
-        raise KeyError('please specify ckpt_path option.')
+    if 'weight' not in cfg.keys():
+        raise KeyError('please specify weight option.')
+    cfg.weight = os.path.join(hydra.utils.get_original_cwd(), cfg.weight)
 
-    print(cfg.pretty())
+    hydra_logger = logging.getLogger(__name__)
+    hydra_logger.info(' '.join(sys.argv))
+    hydra_logger.info(cfg.pretty())
 
-    logger = pytorch_lightning.loggers.mlflow.MLFlowLogger(
-        experiment_name='mlflow_output',
-        tags=None
-    )
+    loggers = [
+        pytorch_lightning.loggers.mlflow.MLFlowLogger(
+            experiment_name='mlflow_output',
+            tags=None)
+    ]
+    api_key = os.environ.get('ONLINE_LOGGER_API_KEY')
+    if api_key and cfg.online_logger.activate:
+        loggers.append(
+            pytorch_lightning.loggers.CometLogger(api_key=api_key)
+        )
 
     # log hyperparams
-    _cfg = copy.deepcopy(cfg)
-    for key, val in cfg.items():
-        if type(val) is omegaconf.dictconfig.DictConfig:
-            dict_for_log = {'.'.join([key, k]): v for k, v in val.items()}  # because cfg is nested dict, the nest info is added to keys.
-            logger.log_hyperparams(dict_for_log)
-            _cfg.pop(key)
-    logger.log_hyperparams(dict(_cfg))
+    for logger in loggers:
+        log_hyperparams(logger, cfg)
 
     trainer = pytorch_lightning.trainer.Trainer(
         deterministic=False,  # IMPORTANT: set True when you need reproductivity.
@@ -41,13 +50,19 @@ def main(cfg: omegaconf.DictConfig) -> None:
         gpus=cfg.gpus,
         max_epochs=cfg.epochs,
         min_epochs=cfg.epochs,
-        logger=logger,
+        logger=loggers,
         default_save_path='.',
         weights_save_path='.'
     )
 
+    model = ModelBuilder(num_classes=cfg.dataset.num_classes, pretrained=False)[cfg.arch]
+    if 'weight' in cfg:
+        print('loading weight from {weight}'.format(weight=cfg.weight))
+        load_model(model, cfg.weight)  # load model weight
+    model.eval()
     # test
-    litmodel = LitModel(cfg).load_from_checkpoint(cfg.ckpt_path, cfg)
+    # litmodel = LitModel(cfg).load_from_checkpoint(model, cfg.ckpt, cfg)
+    litmodel = LitModel(model, cfg)
     trainer.test(litmodel)
 
 
