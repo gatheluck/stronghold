d0d1eb6d02ebef13a75d77623311e4017bf0bc81
gatheluck <gatheluck+tech@gmail.com>
2020-07-04 06:26:06 +0900

    rename v2 version as default

--- apps/train.py
+++ apps/train.py
@@ -4,81 +4,22 @@ import sys
 base = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../')
 sys.path.append(base)
 
-import copy
 import hydra
-import logging
 import omegaconf
-import torch
-import pytorch_lightning
 
-from libs.io import save_model
-from libs.io import log_hyperparams
-from libs.litmodel import LitModel
-
+from libs.trainutil import lightning_train
 from submodules.ModelBuilder.model_builder import ModelBuilder
 
 
-@hydra.main(config_path='../conf/train.yaml', strict=False)
+@hydra.main(config_path='../conf/train.yaml')
 def main(cfg: omegaconf.DictConfig) -> None:
     # fix relative path because hydra automatically change the current working dirctory.
-    cfg.resume_ckpt_path = os.path.join(hydra.utils.get_original_cwd(), cfg.resume_ckpt_path) if (cfg.resume_ckpt_path) and (not cfg.resume_ckpt_path.startswith('/')) else cfg.resume_ckpt_path
+    if (cfg.resume_ckpt_path) and (not cfg.resume_ckpt_path.startswith('/')):
+        cfg.resume_ckpt_path = os.path.join(hydra.utils.get_original_cwd(), cfg.resume_ckpt_path)
 
-    hydra_logger = logging.getLogger(__name__)
-    hydra_logger.info(' '.join(sys.argv))
-    hydra_logger.info(cfg.pretty())
-
-    loggers = [
-        pytorch_lightning.loggers.mlflow.MLFlowLogger(
-            experiment_name='mlflow_output',
-            tags=None)
-    ]
-    api_key = os.environ.get('ONLINE_LOGGER_API_KEY')
-    if api_key and cfg.online_logger.activate:
-        loggers.append(
-            pytorch_lightning.loggers.CometLogger(api_key=api_key)
-        )
-
-    # log hyperparams
-    for logger in loggers:
-        logger.log_hyperparams(omegaconf.DictConfig.to_container(cfg))
-        # log_hyperparams(logger, cfg)
-
-    # this function is called when saving checkpoint
-    checkpoint_callback = pytorch_lightning.callbacks.ModelCheckpoint(
-        filepath=os.path.join(os.getcwd(), 'checkpoint', '{epoch}-{val_loss_avg:.2f}'),
-        monitor=cfg.checkpoint_monitor,
-        save_top_k=1,
-        verbose=True,
-        mode=cfg.checkpoint_mode,
-        save_weights_only=False,
-        prefix=cfg.prefix
-    )
-
-    trainer = pytorch_lightning.trainer.Trainer(
-        deterministic=False,  # set True when you need reproductivity.
-        benchmark=True,  # this will accerarate training.
-        fast_dev_run=False,  # if it is True, run only one batch for each epoch. it is useful for debuging
-        gpus=cfg.gpus,
-        num_nodes=cfg.num_nodes,
-        # distributed_backend=cfg.distributed_backend,  # check https://pytorch-lightning.readthedocs.io/en/stable/trainer.html#distributed-backend
-        max_epochs=cfg.epochs,
-        min_epochs=cfg.epochs,
-        logger=loggers,
-        checkpoint_callback=checkpoint_callback,
-        default_save_path='.',
-        weights_save_path='.',
-        resume_from_checkpoint=cfg.resume_ckpt_path  # if not None, resume from checkpoint
-    )
-
-    # build model
+    # build model and train
     model = ModelBuilder(num_classes=cfg.dataset.num_classes, pretrained=False)[cfg.arch]
-    litmodel = LitModel(model, cfg)
-
-    # train
-    trainer.fit(litmodel)
-    save_model(litmodel.model, os.path.join(os.getcwd(), 'checkpoint', 'model_weight_final.pth'))  # manual backup of final model weight.
-    # test
-    trainer.test()
+    lightning_train(model, cfg)
 
 
 if __name__ == '__main__':
--- apps/transfer.py
+++ apps/transfer.py
@@ -4,92 +4,36 @@ import sys
 base = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../')
 sys.path.append(base)
 
-import copy
-import logging
 import hydra
 import omegaconf
-import torch
-import pytorch_lightning
 
-from libs.io import save_model
+from libs.trainutil import lightning_train
+from libs.utils import UNFREEZE_PARAMS
 from libs.io import load_model
-from libs.io import log_hyperparams
-from libs.litmodel import LitModel
-from libs.utils import check_required_keys
 from libs.utils import replace_final_fc
 from libs.utils import freeze_params
-from libs.utils import UNFREEZE_PARAMS
-
 from submodules.ModelBuilder.model_builder import ModelBuilder
 
 
-@hydra.main(config_path='../conf/transfer.yaml', strict=False)
+@hydra.main(config_path='../conf/transfer.yaml')
 def main(cfg: omegaconf.DictConfig) -> None:
-    # check config
-    required_keys = 'weight original_num_classes'.split()
-    check_required_keys(required_keys, cfg)
+    assert cfg.weight, 'please specify [weight] option.'
+    assert (not cfg.unfreeze_params) and (not cfg.unfreeze_level), 'please specify either [unfreeze_params] or [unfreeze_level]'
 
-    # fix weight bacause hydra change the current working dir
-    cfg.weight = os.path.join(hydra.utils.get_original_cwd(), cfg.weight)
-    cfg.unfreeze_params = UNFREEZE_PARAMS[cfg.arch][cfg.unfreeze_level]
+    # fix relative path because hydra automatically change the current working dirctory.
+    if not cfg.weight.startswith('/'):
+        cfg.weight = os.path.join(hydra.utils.get_original_cwd(), cfg.weight)
 
-    hydra_logger = logging.getLogger(__name__)
-    hydra_logger.info(' '.join(sys.argv))
-    hydra_logger.info(cfg.pretty())
+    # get keys of nufreeze params. if you want specify more detail, please use [unfreeze_params] option directry
+    if not cfg.unfreeze_params:
+        cfg.unfreeze_params = UNFREEZE_PARAMS[cfg.arch][cfg.unfreeze_level]
 
-    loggers = [
-        pytorch_lightning.loggers.mlflow.MLFlowLogger(
-            experiment_name='mlflow_output',
-            tags=None)
-    ]
-    api_key = os.environ.get('ONLINE_LOGGER_API_KEY')
-    if api_key and cfg.online_logger.activate:
-        loggers.append(
-            pytorch_lightning.loggers.CometLogger(api_key=api_key)
-        )
-
-    # log hyperparams
-    for logger in loggers:
-        log_hyperparams(logger, cfg)
-
-    # this function is called when saving checkpoint
-    checkpoint_callback = pytorch_lightning.callbacks.ModelCheckpoint(
-        filepath=os.path.join(os.getcwd(), 'checkpoint', '{epoch}-{val_loss_avg:.2f}'),
-        monitor=cfg.checkpoint_monitor,
-        save_top_k=1,
-        verbose=True,
-        mode=cfg.checkpoint_mode,
-        save_weights_only=False,
-        prefix=cfg.prefix
-    )
-
-    trainer = pytorch_lightning.trainer.Trainer(
-        deterministic=False,  # set True when you need reproductivity.
-        benchmark=True,  # this will accerarate training.
-        gpus=cfg.gpus,
-        max_epochs=cfg.epochs,
-        min_epochs=cfg.epochs,
-        logger=loggers,
-        checkpoint_callback=checkpoint_callback,
-        default_save_path='.',
-        weights_save_path='.'
-    )
-
-    # build model
-    model = ModelBuilder(num_classes=cfg.original_num_classes, pretrained=False)[cfg.arch]
-    if 'weight' in cfg:
-        hydra_logger.info('loading weight from {weight}'.format(weight=cfg.weight))
-        load_model(model, cfg.weight)  # load model weight
+    # build model and train
+    model = ModelBuilder(num_classes=cfg.source_num_classes, pretrained=False)[cfg.arch]
+    load_model(model, cfg.weight)  # load model weight
     replace_final_fc(cfg.arch, model, cfg.dataset.num_classes)  # replace fc
     freeze_params(model, cfg.unfreeze_params)  # freeze some params
-    # build lighting model
-    litmodel = LitModel(model, cfg)
-
-    # train for transfer
-    trainer.fit(litmodel)
-    save_model(litmodel.model, os.path.join(os.getcwd(), 'checkpoint', 'model_weight_transfered_final.pth'))  # manual backup of final model weight.
-    # test
-    trainer.test()
+    lightning_train(model, cfg)
 
 
 if __name__ == '__main__':
