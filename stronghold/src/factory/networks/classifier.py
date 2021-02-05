bf1511f6ebc5b72f4c1d2f9538db8f44f30aa8c4
gatheluck <gatheluck+tech@gmail.com>
2021-02-05 18:00:13 +0900

    feat: add LitClassifier class.

--- stronghold/src/factory/networks/classifier.py
+++ stronghold/src/factory/networks/classifier.py
@@ -1,12 +1,28 @@
+from typing import Dict, Final, Tuple, Union
+
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
+import torchvision
 from hydra.utils import instantiate
+from torch.nn.modules.loss import _Loss
+from torch.optim.lr_scheduler import _LRScheduler
 
+import stronghold.src.common
 import stronghold.src.schema as schema
 
 
 class LitClassifier(pl.LightningModule):
+    """Lightning Module for image classfication.
+
+    Attributes:
+        encoder (nn.Module): The encoder to extract feature for classification.
+        optimizer_cfg (schema.OptimizerConfig): The config for optimizer.
+        scheduler_cfg (schema.SchedulerConfig): The config for sheduler.
+        criterion (_Loss): The loss used by optimizer.
+
+    """
+
     def __init__(
         self,
         encoder: nn.Module,
@@ -17,24 +33,77 @@ class LitClassifier(pl.LightningModule):
         self.encoder = encoder
         self.optimizer_cfg = optimizer_cfg
         self.scheduler_cfg = scheduler_cfg
+        self.criterion: Final[_Loss] = torch.nn.CrossEntropyLoss()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:  # type: ignore
+        """Forward input to encoder to get logit."""
         return self.encoder(x)
 
-    def configure_optimizers(self):
-        # instantiate optimizer.
-        try:
-            optimizer = instantiate(
-                self.optimizer_cfg.instance, params=self.encoder.parameters()
-            )
-        except Exception as e:
-            print(f"unknown error: {e}")
+    def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> Dict[str, Union[torch.Tensor, Dict[str, torch.Tensor]]]:  # type: ignore
+        """Single training step.
 
-        # instantiate scheduler.
-        try:
-            scheduler = instantiate(self.scheduler_cfg.instance)
-        except Exception as e:
-            print(f"unknown error: {e}")
+        Args:
+            batch (Tuple[torch.Tensor, torch.Tensor]): The tuple of input tensor and label.
+            batch_index (int): The index of batch.
 
-        print(optimizer)
-        return [optimizer], [scheduler]
+        Returns:
+            torch.Tensor: The loss tensor.
+
+        """
+        x, t = batch  # DO NOT need to send GPU manually.
+
+        # forward to encoder
+        output = self.encoder(x)
+        loss = self.criterion(output, t)
+
+        # save sample input
+        if batch_idx == 1:
+            torchvision.utils.save_image(x.detach()[:32], "train_img_sample.png")
+
+        # calculate metrics.
+        err1, err5 = stronghold.src.common.calc_errors(
+            output.detach(), t.detach(), topk=(1, 5)
+        )
+
+        # logging loss / top1 err / top5 err
+        self.log("train_loss", loss.detach(), on_epoch=True, sync_dist=True)
+        self.log("train_err1", err1.detach(), on_epoch=True, sync_dist=True)
+        self.log("train_err5", err5.detach(), on_epoch=True, sync_dist=True)
+        return loss
+
+    def validation_step(
+        self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> None:
+        """Single validation step.
+
+        Args:
+            batch (Tuple[torch.Tensor, torch.Tensor]): The tuple of input tensor and label.
+            batch_index (int): The index of batch.
+
+        Returns:
+            Dict[torch.Tensor]: The dict of log info.
+
+        """
+        x, t = batch  # DO NOT need to send GPU manually.
+
+        # forward to encoder
+        output = self.encoder(x)
+        loss = self.criterion(output, t)
+
+        # calculate metrics.
+        err1, err5 = stronghold.src.common.calc_errors(
+            output.detach(), t.detach(), topk=(1, 5)
+        )
+
+        # logging loss / top1 err / top5 err
+        self.log("val_loss", loss.detach(), on_epoch=True, sync_dist=True)
+        self.log("val_err1", err1.detach(), on_epoch=True, sync_dist=True)
+        self.log("val_err5", err5.detach(), on_epoch=True, sync_dist=True)
+
+    def configure_optimizers(
+        self,
+    ) -> Dict[str, Union[torch.optim.Optimizer, _LRScheduler]]:
+        """setup optimzier and scheduler."""
+        optimizer = instantiate(self.optimizer_cfg, params=self.encoder.parameters())
+        scheduler = instantiate(self.scheduler_cfg, optimizer)
+        return dict(optimizer=optimizer, lr_scheduler=scheduler)
